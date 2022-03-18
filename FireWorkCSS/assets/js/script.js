43f756db40c17e1c1ec7f63b39cd7e7f9c6b5d7e
Fernando E. T. Guiarte <enrique.trujillo@alumnos.udg.mx>
2022-03-18 02:39:29 -0600

    Cambios en navbar bastantes significantes, cambios en footer

---
+++ FireWorkCSS/assets/js/script.js
@@ -0,0 +1,8 @@
+var num = 50;
+$(window).bind('scroll', function () {
+    if ($(window).scrollTop() > num) {
+        $('.menu').addClass('fixed');
+    } else {
+        $('.menu').removeClass('fixed');
+    }
+});
