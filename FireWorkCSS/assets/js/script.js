92dd3dc5f0f67d045b2aed866dd02062138ae980
student420-69 <diego.hgonzalez@alumnos.udg.mx>
2022-03-19 20:24:17 -0600

    Creacion modo oscuro, se añadieron algunos de los componentes a la pagina de inicio

--- FireWorkCSS/assets/js/script.js
+++ FireWorkCSS/assets/js/script.js
@@ -26,3 +26,12 @@ function myFunction() {
 
 /*var navBarMov = $('#myTopnav').html();
 $('#navBarMov').html('<ul>'+navBarMov+'</ul>');*/
+const colorSwitch = document.querySelector('#switch input[type="checkbox"]');
+            function cambiaTema(ev){
+                if(ev.target.checked){
+                    document.documentElement.setAttribute('tema', 'light');
+                } else {
+                    document.documentElement.setAttribute('tema', 'dark');
+                }
+            }
+            colorSwitch.addEventListener('change', cambiaTema);
